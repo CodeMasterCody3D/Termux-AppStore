f668e74ba618711ccd1e288452a9501a990a8250
Cody D Dixon <130433943+CodeMasterCody3D@users.noreply.github.com>
2025-03-24 11:28:16 -0500

    Update uninstall.sh

--- apps/OrcaSlicer/uninstall.sh
+++ apps/OrcaSlicer/uninstall.sh
@@ -1,3 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 package_remove_and_check "OrcaSlicer"
+
+# Uninstall OrcaSlicer
+pdrun sudo apt remove -y orcaslicer
+pdrun sudo apt autoremove -y
+
+echo "OrcaSlicer has been uninstalled."
